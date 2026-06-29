a18f7bf6f75cfe35b3f201c943fe2518c3704c86
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2026-06-29 03:40:06 -0700

    increase clock skew checker to 5000

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2111,7 +2111,7 @@
 		{/if}
 	</div>
 
-	{#if clock_skew !== null && Math.abs(clock_skew) > 2000 && !clock_skew_ignored}
+	{#if clock_skew !== null && Math.abs(clock_skew) > 5000 && !clock_skew_ignored}
 		<div
 			class="hidden md:flex fixed top-4 left-[396px] right-20 px-4 py-2 border rounded shadow-md text-sm justify-between items-center z-40 {Math.abs(
 				clock_skew
